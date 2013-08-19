4b137b4c414c9742ca3141b9c5d653dba298285b
Jackson Teixeira <jackson.s.teixeira@gmail.com>
2013-08-19 18:08:29 -0300

    add files

--- app/index.php
+++ app/index.php
@@ -1,3 +1,61 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<title>bf</title>
+<link href="assets/stylesheets/reset.css" media="all" rel="stylesheet" type="text/css" />
+<link href="assets/stylesheets/bootstrap.min.css" media="all" rel="stylesheet" type="text/css" />
+<link href="assets/stylesheets/application.css" media="all" rel="stylesheet" type="text/css" />
+</head>
 
-	echo "<h1>ok</h1>";
+<body>
+
+<div class="container-fluid">
+
+	<div class="row-fluid">
+		<div class="span12">
+			<div id="legend">
+				<h2>bf</h2>
+			</div>
+		</div>
+	</div>
+   
+	<div class="row-fluid">
+		<div class="span6">
+		-
+		</div>
+		<div class="span6">
+		<form class="form-horizontal" action='' method="POST">
+			<fieldset>
+				<div class="control-group">
+					<!-- Username -->
+					<label class="control-label"  for="username">Username</label>
+					<div class="controls">
+						<input type="text" id="username" name="username" placeholder="" class="input-xlarge">
+					</div>
+				</div>
+				
+				<div class="control-group">
+					<!-- Password-->
+					<label class="control-label" for="password">Password</label>
+					<div class="controls">
+						<input type="password" id="password" name="password" placeholder="" class="input-xlarge">
+					</div>
+				</div>
+				
+				
+				<div class="control-group">
+					<!-- Button -->
+					<div class="controls">
+						<button class="btn btn-success">Login</button>
+					</div>
+				</div>
+				
+			</fieldset>
+			</form>
+		</div>
+	</div>
+</div>
+
+
+</body>
+</html>
